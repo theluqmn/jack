9f43622ae212fe23519bda17daf2e813ec95090a
luq-mne <luqman.yazrie@outlook.com>
2025-05-25 14:19:24 +0800

    made things to functions and stuff, more work will prevail later

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 import random, time
 
-sum = 0
-for i in range(4):
-    number = random.randint(0, 15)
-    print(f"+\t{number}")
-    sum += number
+def generate_question():
+    total = 0
+    for i in range(4):
+        number = random.randint(0, 15)
+        print(f"+\t{number}")
+        total += number
 
-time_start = time.time()
-answer = input("----------\nSum:\t")
-time_end = time.time()
-time_taken = (time_end - time_start).__round__(2)
+    time_start = time.time()
+    answer = input("----------\ntotal:\t")
+    time_end = time.time()
+    time_taken = (time_end - time_start).__round__(2)
 
-if answer == str(sum):
-    print("Correct!")
-else:
-    print(f"Incorrect!\nAnswer: {sum}")
+    if answer == str(total):
+        print("Correct!")
+    else:
+        print(f"Incorrect!\nAnswer: {total}")
 
-print(f"\nTime taken: {time_taken}s")
+    return answer, total, time_taken
+
+stats = {
+    "answer": [],
+    "total": [],
+    "time": []
+}
+
+for i in range(10):
+    print(f"\nQuestion {i+1}:")
+    answer, total, time_taken = generate_question()
+    print(f"Time taken: {time_taken}s")
+    stats["answer"].append(answer == str(total))
+    stats["total"].append(total)
+    stats["time"].append(time_taken)
+
+print("\nResults:")
+time_average = sum(stats["time"]) / len(stats["time"])
+percentage_correct = (stats["answer"].count(True) / len(stats["answer"])) * 100
+print(f"Average time: {time_average}s")
+print(f"Percentage correct: {percentage_correct}%")
