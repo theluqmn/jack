052c0c94dd1a259025be9c4d1608157f5032c1e4
luq-mne <luqman.yazrie@outlook.com>
2025-05-25 11:04:19 +0800

    added time tracker

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import random
+import random, time
 
 sum = 0
 for i in range(4):
     number = random.randint(0, 15)
-    print(f"+\t{number}")
+    print(f"+ {number}")
     sum += number
 
+time_start = time.time()
 answer = input("Enter the sum: ")
+time_end = time.time()
+time_taken = (time_end - time_start).__round__(2)
 if answer == str(sum):
     print("Correct!")
 else:
     print("Incorrect!")
+print(f"Time taken: {time_taken}s")
