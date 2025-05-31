7b7881118aa95af472af5f737bc8ba3dc8564791
luq-mne <luqman.yazrie@outlook.com>
2025-06-01 06:57:12 +0800

    restructured things

--- main.py
+++ main.py
@@ -1,46 +1,10 @@
 import random, time
 
-def generate_question():
-    num_range_min = 0
-    num_range_max = 15
-    
-    total = 0
-    for i in range(4):
-        number = random.randint(num_range_min, num_range_max)
-        print(f"+\t{number}")
-        total += number
-
-    time_start = time.time()
-    answer = input("----------\ntotal:\t")
-    time_end = time.time()
-    time_taken = (time_end - time_start).__round__(2)
-
-    if answer == str(total):
-        print("Correct!")
-    else:
-        print(f"Incorrect!\nAnswer: {total}")
-
-    return answer, total, time_taken
-
-stats = {
-    "answer": [],
-    "total": [],
-    "time": []
-}
-
-for i in range(10):
-    print(f"\nQuestion {i+1}:")
-    answer, total, time_taken = generate_question()
-    print(f"Time taken: {time_taken}s")
-    stats["answer"].append(answer == str(total))
-    stats["total"].append(total)
-    stats["time"].append(time_taken)
-
 def generate_questions(num_questions, num_length, num_range_min, num_range_max):
     questions = []
 
     for i in range(num_questions):
-        question = { "numbers": [], "answer": 0 }
+        question = { "numbers": [], "answer": 0, "time": 0, "correct": False }
 
         for i in range(num_length):
             number = random.randint(num_range_min, num_range_max)
@@ -49,8 +13,38 @@ def generate_questions(num_questions, num_length, num_range_min, num_range_max):
 
     return questions
 
-print("\nResults:")
-time_average = sum(stats["time"]) / len(stats["time"])
-percentage_correct = (stats["answer"].count(True) / len(stats["answer"])) * 100
-print(f"Average time: {time_average}s")
-print(f"Percentage correct: {percentage_correct}%")
+def prompt_question(number, question):
+    print(f"\nquestion {number}:")
+
+    for i in range(len(question["numbers"])):
+        if i < 0:
+            print(f"-\t{question['numbers'][i]}")
+        else:
+            print(f"+\t{question['numbers'][i]}")
+    time_start = time.time()
+    answer = input("----------\ntotal:\t")
+    time_end = time.time()
+    time_taken = (time_end - time_start).__round__(2)
+    print(f"Time taken: {time_taken}s")
+
+    if answer == str(question["answer"]):
+        print("correct!")
+        question["correct"] = True
+    else:
+        print(f"incorrect!\nanswer: {question['answer']}")
+
+    return answer, time_taken
+
+if __name__ == "__main__":
+    questions = generate_questions(10, 4, 0, 15)
+    for i in range(len(questions)):
+        answer, time_taken = prompt_question(i+1, questions[i])
+        questions[i]["answer"] = answer
+        questions[i]["time"] = time_taken
+
+    time_average = sum([question["time"] for question in questions]) / len(questions)
+    percentage_correct = (sum([question["correct"] for question in questions]) / len(questions)) * 100
+
+    print("\nResults:")
+    print(f"Average time: {time_average}s")
+    print(f"Percentage correct: {percentage_correct}% ({sum([question['correct'] for question in questions])}/{len(questions)})")
